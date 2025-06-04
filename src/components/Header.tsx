9f8160012d036d953e2d8c661b6ab69db094b215
Zipora5572 <z0548545572@gmail.com>
2025-06-04 17:59:22 +0300

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ const Header = ({ isPublicPage }: HeaderProps) => {
 
   return (
     <header className="w-full px-4 md:px-6 py-5 border-b shadow-sm bg-white z-50">
-    <div className="flex items-center justify-between max-w-7xl mx-auto">
+    <div className="flex items-center justify-between mx-auto">
       {/* לוגו */}
       <Link to="/" className="flex items-center gap-3">
         <div className="flex h-12 w-12 items-center justify-center rounded-md bg-red-600 text-white shadow-sm">
